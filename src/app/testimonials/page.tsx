5dbd65d83138fad7765f52525699fbdf3e4eb663
sheylavperez <sheylavperez@gmail.com>
2025-08-19 00:07:34 -0400

    fix lint/build issues

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
