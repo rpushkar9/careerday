f9c6f097877493443d17df3e0d68c35744428269
sheylavperez <sheylavperez@gmail.com>
2025-08-29 13:53:49 -0400

    change name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
-  title: 'CareerDay',
+  title: 'CareerDayy',
   description: 'Created with love',
   generator: 'dev',
   icons: {
