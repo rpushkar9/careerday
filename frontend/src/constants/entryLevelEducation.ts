9e8c5f584807998d5bb3d997edc099a9d540ec66
Sophie Lin <98501518+soph-lin@users.noreply.github.com>
2025-08-29 10:19:58 -0500

    Add entry level education types

---
+++ frontend/src/constants/entryLevelEducation.ts
@@ -0,0 +1,12 @@
+const ENTRY_LEVEL_EDUCATION = [
+    'Doctoral or professional degree',
+    'Master\'s degree',
+    'Bachelor\'s degree',
+    'Associate\'s degree',
+    'Postsecondary nondegree award',
+    'Some college, no degree',
+    'High school diploma or equivalent',
+    'No formal educational credential',
+]
+
+export default ENTRY_LEVEL_EDUCATION;
