c85b6e99293f93b9423dde76bc8d615d528e4bad
Sophie Lin <98501518+soph-lin@users.noreply.github.com>
2025-08-29 10:23:17 -0500

    Add employment data to backend

---
+++ backend/src/constants/employmentProjection.py
@@ -0,0 +1,20 @@
+"""
+Data information for BLS Employment Projections: https://www.bls.gov/emp/documentation/definitions.htm
+"""
+
+ENTRY_LEVEL_EDUCATION = [
+    'Doctoral or professional degree',
+    'Master\'s degree',
+    'Bachelor\'s degree',
+    'Associate\'s degree',
+    'Postsecondary nondegree award',
+    'Some college, no degree',
+    'High school diploma or equivalent',
+    'No formal educational credential',
+]
+
+WORK_EXPERIENCE_REQUIRED = [
+    '5 years or more',
+    'Less than 5 years',
+    'None',
+]
